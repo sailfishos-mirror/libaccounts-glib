18c306f31e3cd7f069f553c29eca24313a95e8c2
Alberto Mardegan <alberto.mardegan@nokia.com>
2009-08-17 10:05:34 +0300

    Implement ag_account_list_services_by_type

--- libaccounts-glib/ag-account.c
+++ libaccounts-glib/ag-account.c
@@ -694,9 +694,33 @@ GList *
 ag_account_list_services_by_type (AgAccount *account,
                                   const gchar *service_type)
 {
+    AgAccountPrivate *priv;
+    GList *all_services, *list;
+    GList *services = NULL;
+
     g_return_val_if_fail (AG_IS_ACCOUNT (account), NULL);
-    g_warning ("%s not implemented", G_STRFUNC);
-    return NULL;
+    g_return_val_if_fail (service_type != NULL, NULL);
+    priv = account->priv;
+
+    if (!priv->provider_name)
+        return NULL;
+
+    all_services = ag_manager_list_services (priv->manager);
+    for (list = all_services; list != NULL; list = list->next)
+    {
+        AgService *service = list->data;
+
+        if (service->provider && service->type &&
+            strcmp (service->provider, priv->provider_name) == 0 &&
+            strcmp (service->type, service_type) == 0)
+        {
+            services = g_list_prepend (services, service);
+        }
+        else
+            ag_service_unref (service);
+    }
+    g_list_free (all_services);
+    return services;
 }
 
 /**
