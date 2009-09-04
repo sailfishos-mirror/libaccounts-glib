4a15dca780786f5c671290cd9e6adb030d7ddb9a
Alberto Mardegan <alberto.mardegan@nokia.com>
2009-09-04 15:02:37 +0300

    Use LGPL copyright

--- tests/check_ag.c
+++ tests/check_ag.c
@@ -1,7 +1,25 @@
-/**
+/* vi: set et sw=4 ts=4 cino=t0,(0: */
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of libaccounts-glib
+ *
  * Copyright (C) 2009 Nokia Corporation.
+ *
  * Contact: Alberto Mardegan <alberto.mardegan@nokia.com>
- * Licensed under the terms of Nokia EUSA (see the LICENSE file)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
  */
 
 /**
